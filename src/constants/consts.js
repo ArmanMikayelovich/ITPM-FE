d07078e20957e015daea7853600f21dd15038029
Arman Arshakyan <armanmikayelovich@gmail.com>
2020-08-28 21:41:07 +0400

    created board part.

--- src/constants/consts.js
+++ src/constants/consts.js
@@ -3,3 +3,11 @@ export  const HOST_ADDRESS = "http://localhost:8080"
 export const GOOGLE_AUTH = HOST_ADDRESS + '/oauth2/authorization/google';
 export const GITHUB_AUTH = HOST_ADDRESS + '/oauth2/authorization/github';
 
+export const TASK_TODO = "TODO";
+export const TASK_IN_PROGRESS = "IN_PROGRESS";
+export  const TASK_DONE = "DONE"
+
+
+export const LIST_TODO = "To do";
+export const LIST_IN_PROGRESS = "In Progress";
+export const LIST_DONE = "Done"
--- src/project/ProjectPage.js
+++ src/project/ProjectPage.js
@@ -1,14 +1,21 @@
 import React, {useLayoutEffect, useState} from "react";
 import {useLocation} from "react-router";
 import {UserFullName} from "../user/UserInfo";
-import {HOST_ADDRESS} from "../constants/consts";
+import {
+    HOST_ADDRESS,
+    LIST_DONE,
+    LIST_IN_PROGRESS,
+    LIST_TODO,
+    TASK_DONE,
+    TASK_IN_PROGRESS,
+    TASK_TODO
+} from "../constants/consts";
 
-export function ProjectPage(props) {
+export function ProjectPage() {
 
     const location = useLocation()
 
     const project = location.project
-    let usersInProject = [];
 
     return (
         <div>
@@ -19,6 +26,52 @@ export function ProjectPage(props) {
                 <UsersInProjectList projectId={project.id}/>
             </div>
 
+            <br/>
+
+            <Board projectId={project.id} />
+        </div>
+    );
+}
+
+function Board(props) {
+    const projectId = props.projectId;
+    const [sprint, setSprint] = useState(null);
+
+    useLayoutEffect(() => {
+        fetch(HOST_ADDRESS + `/sprints/by-project/${projectId}/active`, {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+
+            },
+        }).then((response) => {
+
+                if (response.status === 200) {
+                    let json = response.json();
+                    json.then(data => {
+                        setSprint(data);
+                    });
+                }
+
+            }
+        ).catch(error => console.log(`an error occurred ${error}`));
+    }, [projectId])
+
+
+    if(!sprint) {
+        return (
+            <div><h4>You haven't started a sprint</h4></div>
+        )
+    }
+
+    return (
+        <div>
+            <TaskList sprintId={sprint.id} taskState={TASK_TODO} listType={LIST_TODO}/>
+            <TaskList sprintId={sprint.id} taskState={TASK_IN_PROGRESS} listType={LIST_IN_PROGRESS}/>
+            <TaskList sprintId={sprint.id} taskState={TASK_DONE} listType={LIST_DONE}/>
         </div>
     )
 }
@@ -51,14 +104,57 @@ function UsersInProjectList(props) {
         ).catch(error => console.log(`an error occurred ${error}`));
 
 
-    }, [])
+    }, [projectId])
 
     return (
         <div>
-            <ul>
-                {users}
-            </ul>
+            <hx> Users in Project
+                <ul>
+                    {users}
+                </ul></hx>
         </div>
 
     )
+}
+
+function TaskList(props) {
+    const sprintId = props.sprintId;
+    const taskState = props.taskState;
+    const listType = props.listType;
+    const [tasks, setTasks] = useState([]);
+
+    useLayoutEffect(() => {
+        fetch(HOST_ADDRESS + '/tasks/by-sprint/' + sprintId + "?taskState=" + taskState, {
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+
+            },
+        }).then((response) => {
+
+                let json = response.json();
+                json.then(data => {
+                    let taskList = data.map(task => <li key={task.id}>{task.name + " " + task.taskType}</li>)
+                    setTasks(taskList);
+                });
+            }
+        ).catch(error => console.log(`an error occurred ${error}`));
+    }, [sprintId, taskState])
+
+    return (
+        <div style={{
+            display: "inline-block",
+            'margin-right': '10px'
+        }}>
+            <hx>{listType}
+                <ul>
+                    {tasks}
+                </ul>
+            </hx>
+
+        </div>
+    );
 }
